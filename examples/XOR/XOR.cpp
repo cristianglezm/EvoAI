3f0617cb77bed761dc97a4f99ef49e591cfd6170
Cristian González <cristian.glez.m@gmail.com>
2016-01-04 14:58:03 +0100

    added basic XOR

--- examples/XOR/XOR.cpp
+++ examples/XOR/XOR.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <EvoAI.hpp>
+#include <utility>
+#include <memory>
+#include <vector>
 
 int main(){
-    std::cout << "Testing XOR..." << std::endl;
+    using namespace EvoAI;
+    std::unique_ptr<NeuralNetwork> nn = createXORNeuralNetwork();
+    double x[4] = {0.0,0.0,1.0,1.0};
+    double y[4] = {0.0,1.0,0.0,1.0};
+    double truth[4] = {0.0,1.0,1.0,0.0};
+    for(auto i=0u;i<4;++i){
+        std::vector<double> inputs;
+        inputs.emplace_back(x[i]);
+        inputs.emplace_back(y[i]);
+        nn->setInputs(std::move(inputs));
+        auto res = nn->run();
+        std::cout << "x: " << x[i] << ", y: " << y[i] << " : answer: " << res[0] << " Correct Answer: " << truth[i] << std::endl;
+        nn->reset();
+    }
     return 0;
 }
