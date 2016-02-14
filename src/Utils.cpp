b599b9b0a6a9d06f7b8fc6d8ec3adbbaf2184200
Cristian González <cristian.glez.m@gmail.com>
2016-02-14 13:57:49 +0100

    Modified Cmake, utils.cpp
    Changed the way genererateSound works. testing it
    Changed cmake

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -451,7 +451,6 @@ namespace EvoAI{
         sf::Int16 samplesBuffer[height];
         sf::Uint64 count = height;
         for(auto x=0;x<width;++x){
-            auto counter = 0;
             for(auto y=0;y<height;++y){
                 auto norm_x = (2*(x/width))-1;
                 auto norm_y = (2*(y/height))-1;
@@ -467,17 +466,10 @@ namespace EvoAI{
                 nn->setInputs(std::move(inputs));
                 auto audio = nn->run();
                 nn->reset();
-                if(counter > 2){
-                    counter = 0;
-                }
-                if(counter == 0){
-                    samplesBuffer[y] = static_cast<sf::Int16>(10000 * (x*y) * std::cosh(audio[counter] * 440 * (audio[counter+1] * 3.1415f) * audio[counter] / 44100));
-                }else if(counter == 1){
-                    samplesBuffer[y] = static_cast<sf::Int16>(10000 * (y*d) * std::sin(audio[counter] * 440 * (audio[counter-1] * 3.1415f) * audio[counter] / 44100));
-                }else if(counter == 2){
-                    samplesBuffer[y] = static_cast<sf::Int16>(10000 * (x*d) * std::cos(audio[counter] * 440 * (audio[counter] * 3.1415f) * audio[counter] / 44100));
-                }
-                ++counter;
+                const int Amplitude = 30000;
+                /// TODO
+                samplesBuffer[y] = static_cast<sf::Int16>(Amplitude * std::tanh((audio[0] + audio[1] + audio[2]) * (2 * 3.141516)));
+                //samplesBuffer[y] = static_cast<sf::Int16>(Amplitude * sf::Color(audio[0] * 255, audio[1] * 255, audio[2] * 255).toInteger());
             }
             osf.write(samplesBuffer,count);
         }
