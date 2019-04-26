ddc847092583b7436f5b469751d5a27208305aeb
Cristian González <cristian.glez.m@gmail.com>
2019-04-27 01:03:59 +0200

    removed do..while in trainXOR

--- examples/XOR/XOR.cpp
+++ examples/XOR/XOR.cpp
@@ -97,20 +97,18 @@ void trainXOR(EvoAI::NeuralNetwork& nn){
     std::vector<double> x = {0.0,0.0,1.0,1.0};
     std::vector<double> y = {0.0,1.0,0.0,1.0};
     std::vector<double> truth = {0.0,1.0,1.0,0.0};
-    do{
-        std::vector<std::vector<double>> inputs;
-        std::vector<std::vector<double>> outputs;
-        for(auto i=0u;i<4;++i){
-            std::vector<double> in;
-            std::vector<double> out;
-            in.emplace_back(x[i]);
-            in.emplace_back(y[i]);
-            out.emplace_back(truth[i]);
-            inputs.emplace_back(in);
-            outputs.emplace_back(out);
-        }
-        nn.train(std::move(inputs),std::move(outputs),0.7,0.3,50);
-    }while(nn.getMSE() > 0.01);
+    std::vector<std::vector<double>> inputs;
+    std::vector<std::vector<double>> outputs;
+    for(auto i=0u;i<4;++i){
+        std::vector<double> in;
+        std::vector<double> out;
+        in.emplace_back(x[i]);
+        in.emplace_back(y[i]);
+        out.emplace_back(truth[i]);
+        inputs.emplace_back(in);
+        outputs.emplace_back(out);
+    }
+    nn.train(std::move(inputs),std::move(outputs),0.7,0.3,5000);
 }
 void testXOR(EvoAI::NeuralNetwork& nn){
     std::vector<double> x = {0.0,0.0,1.0,1.0};
